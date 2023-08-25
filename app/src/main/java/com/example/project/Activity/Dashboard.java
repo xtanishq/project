6a885f1f6ffa10ca41e6ea4a9da718d34cca9429
Tanishq <tanishqkmr@gmail.com>
2023-08-25 14:29:25 +0530

    completed

---
+++ app/src/main/java/com/example/project/Activity/Dashboard.java
@@ -0,0 +1,14 @@
+package com.example.project.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.project.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
