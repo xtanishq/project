5731745b84dbfca3e92be473864e09c56190b02f
Tanishq <tanishqkmr@gmail.com>
2023-08-25 15:48:42 +0530

    aadha cmplt

--- app/src/main/java/com/example/project/Activity/Dashboard.java
+++ app/src/main/java/com/example/project/Activity/Dashboard.java
@@ -2,13 +2,30 @@ package com.example.project.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.example.project.R;
 
 public class Dashboard extends AppCompatActivity {
 
+
+    TextView edfullname,edemail,edpassword,eddob,edgender;
+
+    TextView fullname,email,password,dob,gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        fullname= findViewById(R.id.name);
+        email= findViewById(R.id.email);
+        gender =findViewById(R.id.gender);
+        dob= findViewById(R.id.dob);
+        fullname.setText(Common.getName(Dashboard.this));
+        email.setText(Common.getEmail(Dashboard.this));
+        gender.setText(Common.getGender(Dashboard.this));
+        dob.setText(Common.getDob(Dashboard.this));
+
     }
 }
--- app/src/main/java/com/example/project/Activity/Login.java
+++ app/src/main/java/com/example/project/Activity/Login.java
@@ -2,18 +2,22 @@ package com.example.project.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.project.R;
 
 public class Login extends AppCompatActivity {
     EditText edemail,edpassword;
     Button login;
+    TextView txt;
     SharedPreferences sp;
     SharedPreferences.Editor editor;
     String email,password;
@@ -21,11 +25,17 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        edemail= findViewById(R.id.email);
-        edpassword =findViewById(R.id.password);
+        edemail= findViewById(R.id.lemail);
+        edpassword =findViewById(R.id.lpassword);
+        txt =findViewById(R.id.txt);
         login= findViewById(R.id.login);
-
-
+        Toast.makeText(this, Common.getEmail(Login.this) + " " + Common.getPassword(Login.this), Toast.LENGTH_SHORT).show();
+txt.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(Login.this,SignUp.class));
+    }
+});
 login.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
@@ -45,8 +55,20 @@ login.setOnClickListener(new View.OnClickListener() {
 
         email=edemail.getText().toString().trim();
         password=edpassword.getText().toString().trim();
+verifyUser();
 
+    }
 
+    private void verifyUser() {
+
+        if (email == Common.getEmail(Login.this) && password == Common.getPassword(Login.this)) {
+
+            Toast.makeText(this, "wrong details", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            startActivity(new Intent(Login.this, Dashboard.class));
+
+        }
     }
 
 
