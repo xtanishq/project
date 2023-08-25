4d2b758706246a0d978819a56ba3518aaf60c6e8
Tanishq <tanishqkmr@gmail.com>
2023-08-25 16:10:07 +0530

    completed

--- app/src/main/java/com/example/project/Activity/Dashboard.java
+++ app/src/main/java/com/example/project/Activity/Dashboard.java
@@ -1,9 +1,15 @@
 package com.example.project.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.project.R;
 
@@ -11,7 +17,7 @@ public class Dashboard extends AppCompatActivity {
 
 
     TextView edfullname,edemail,edpassword,eddob,edgender;
-
+Button delete,update;
     TextView fullname,email,password,dob,gender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,8 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         fullname= findViewById(R.id.name);
+        delete= findViewById(R.id.delete);
+        update= findViewById(R.id.update);
         email= findViewById(R.id.email);
         gender =findViewById(R.id.gender);
         dob= findViewById(R.id.dob);
@@ -26,6 +34,22 @@ public class Dashboard extends AppCompatActivity {
         email.setText(Common.getEmail(Dashboard.this));
         gender.setText(Common.getGender(Dashboard.this));
         dob.setText(Common.getDob(Dashboard.this));
+update.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+      startActivity(new Intent(Dashboard.this,Update.class));
+    }
+});
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit().clear().apply();
+                Toast.makeText(Dashboard.this, Common.getName(Dashboard.this)+ "Data deleted", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Dashboard.this,SignUp.class));
+            }
+        });
 
     }
+
+
 }
--- app/src/main/java/com/example/project/Activity/Update.java
+++ app/src/main/java/com/example/project/Activity/Update.java
@@ -2,15 +2,66 @@ package com.example.project.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.project.R;
 
 public class Update extends AppCompatActivity {
 
+
+    EditText edfullname,edemail,edpassword,eddob,edgender;
+    Button update;
+    String fullname,email,password,dob,gender;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        edfullname= findViewById(R.id.fullName);
+        edemail= findViewById(R.id.email);
+        edpassword =findViewById(R.id.password);
+        eddob= findViewById(R.id.dob);
+        update= findViewById(R.id.update);
+
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editTextToString();
+            }
+        });
+    }
+
+    private void editTextToString() {
+
+        fullname=edfullname.getText().toString().trim();
+        email=edemail.getText().toString().trim();
+        password=edpassword.getText().toString().trim();
+        dob=eddob.getText().toString().trim();
+        update();
+
+    }
+
+    private void update() {
+        PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit().clear().apply();
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(Update.this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("name",fullname);
+        editor.putString("email",email);
+        editor.putString("password",password);
+        editor.putString("gender",gender);
+        editor.putString("dob",dob);
+        editor.apply();
+startActivity(new Intent(Update.this,Dashboard.class));
     }
 }
